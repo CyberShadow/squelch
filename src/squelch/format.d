7e6af4f6654ebb7cf71d4ad4f78626c608cc3876
Vladimir Panteleev <git@cy.md>
2021-10-17 08:56:08 +0000

    squelch.format: Refactor to allow multiple passes

--- src/squelch/format.d
+++ src/squelch/format.d
@@ -12,8 +12,6 @@ import squelch.common;
 
 Token[] format(const scope Token[] tokens)
 {
-	Token[] result;
-
 	enum WhiteSpace
 	{
 		none,
@@ -21,263 +19,276 @@ Token[] format(const scope Token[] tokens)
 		newLine,
 		blankLine,
 	}
+	// whiteSpace[i] is what whitespace we should add before tokens[i]
+	auto whiteSpace = new WhiteSpace[tokens.length + 1];
+	auto indent = new size_t[tokens.length];
 
-	WhiteSpace wsLast;
-	bool wasWord;
-	string[] stack;
-
-	foreach (tokenIndex, ref token; tokens)
+	// First pass
 	{
-		WhiteSpace wsPre, wsPost;
-		void delegate()[] post;
-		bool isWord, outdent;
+		bool wasWord;
+		string[] stack;
 
-		token.match!(
-			(ref const TokenWhiteSpace t)
-			{
-				assert(false);
-			},
-			(ref const TokenComment t)
-			{
-				wsPre = wsPost = WhiteSpace.newLine;
-			},
-			(ref const TokenKeyword t)
-			{
-				isWord = true;
-				switch (t.text)
-				{
-					case "AS":
-					case "AND":
-					case "OR":
-					case "NOT":
-					case "LIKE":
-					case "BETWEEN":
-					case "IN":
-					case "IS":
-					case "OVER":
-					case "THEN":
-					case "RETURNS":
-						wsPre = wsPost = WhiteSpace.space;
-						break;
-					case "SELECT":
-					case "SELECT AS STRUCT":
-						wsPre = wsPost = WhiteSpace.newLine;
-						if (stack.endsWith("WITH"))
-							stack.popBack();
-						post ~= { stack ~= "SELECT"; };
-						break;
-					case "FROM":
-					case "WHERE":
-					case "JOIN":
-					case "CROSS JOIN":
-					case "INNER JOIN":
-					case "LEFT JOIN":
-					case "LEFT OUTER JOIN":
-					case "RIGHT JOIN":
-					case "RIGHT OUTER JOIN":
-					case "GROUP BY":
-					case "ORDER BY":
-					case "HAVING":
-					case "QUALIFY":
-					case "PARTITION BY":
-						wsPre = wsPost = WhiteSpace.newLine;
-						if (stack.endsWith("SELECT"))
-							stack.popBack();
-						post ~= { stack ~= "SELECT"; };
-						break;
-					case "WINDOW":
-						wsPre = WhiteSpace.newLine;
-						if (stack.endsWith("SELECT"))
-							stack.popBack();
-						post ~= { stack ~= "SELECT"; };
-						break;
-					case "UNION ALL":
-					case "UNION DISTINCT":
-					case "INTERSECT DISTINCT":
-					case "EXCEPT DISTINCT":
-						wsPre = wsPost = WhiteSpace.newLine;
-						if (stack.endsWith("SELECT"))
-							stack.popBack();
-						outdent = true;
-						break;
-					case "WITH":
-						wsPre = wsPost = WhiteSpace.newLine;
-						post ~= { stack ~= "WITH"; };
-						break;
-					case "USING":
-						wsPre = WhiteSpace.newLine;
-						break;
-					case "CASE":
-						wsPre = wsPost = WhiteSpace.newLine;
-						post ~= { stack ~= "CASE"; };
-						break;
-					case "WHEN":
-					case "ELSE":
-						wsPre = WhiteSpace.newLine;
-						break;
-					case "END":
-						wsPre = WhiteSpace.newLine;
-						if (stack.endsWith("CASE"))
-							stack.popBack();
-						break;
-					default:
-						break;
-				}
-			},
-			(ref const TokenIdentifier t)
-			{
-				isWord = true;
-			},
-			(ref const TokenNamedParameter t)
-			{
-				isWord = true;
-			},
-			(ref const TokenOperator t)
-			{
-				switch (t.text)
-				{
-					case ".":
-						break;
-					case "(":
-						if (tokenIndex + 1 < tokens.length && tokens[tokenIndex + 1] == Token(TokenOperator(")")))
-							return; // Don't break up "()"
-						wsPost = WhiteSpace.newLine;
-						post ~= { stack ~= "("; };
-						break;
-					case ")":
-						if (tokenIndex && tokens[tokenIndex - 1] == Token(TokenOperator("(")))
-							return; // Don't break up "()"
-
-						wsPre = WhiteSpace.newLine;
-						stack = retro(find(retro(stack), "("));
-						enforce(stack.length, "Mismatched )");
-						stack = stack[0 .. $-1];
-						break;
-					case "[":
-						wsPost = WhiteSpace.newLine;
-						post ~= { stack ~= "["; };
-						break;
-					case "]":
-						wsPre = WhiteSpace.newLine;
-						stack = retro(find(retro(stack), "["));
-						enforce(stack.length, "Mismatched ]");
-						stack = stack[0 .. $-1];
-						break;
-					case ",":
-						if (stack.endsWith("<"))
-							wsPost = WhiteSpace.space;
-						else
-						if (stack.endsWith("WITH"))
-							wsPost = WhiteSpace.blankLine;
-						else
-							wsPost = WhiteSpace.newLine;
-						break;
-					case ";":
-						wsPost = WhiteSpace.blankLine;
-						while (stack.endsWith("SELECT") || stack.endsWith("WITH"))
-							stack.popBack();
-						break;
-					default:
-						// Binary operators and others
-						wsPre = wsPost = WhiteSpace.space;
-						break;
-				}
-			},
-			(ref const TokenAngleBracket t)
-			{
-				final switch (t.text)
-				{
-					case "<":
-						post ~= { stack ~= "<"; };
-						break;
-					case ">":
-						stack = retro(find(retro(stack), "<"));
-						enforce(stack.length, "Mismatched >");
-						stack = stack[0 .. $-1];
-						break;
-				}
-			},
-			(ref const TokenString t)
-			{
-				isWord = true;
-			},
-			(ref const TokenNumber t)
-			{
-				isWord = true;
-			},
-			(ref const TokenDbtStatement t)
-			{
-				wsPre = wsPost = WhiteSpace.newLine;
-				switch (t.kind)
-				{
-					case "for":
-					case "if":
-					case "macro":
-					case "filter":
-						post ~= { stack ~= "%" ~ t.kind; };
-						break;
-					case "set":
-						if (!t.text.canFind('='))
-							post ~= { stack ~= "%" ~ t.kind; };
-						break;
-					case "elif":
-					case "else":
-						while (stack.length && !stack[$-1].startsWith("%")) stack.popBack();
-						enforce(stack.endsWith("%if"),
-							"Found " ~ t.kind ~ " but expected " ~ (stack.length ? "end" ~ stack[$-1][1..$] : "end-of-file")
-						);
-						stack.popBack();
-						post ~= { stack ~= "%if"; };
-						break;
-					case "endfor":
-					case "endif":
-					case "endmacro":
-					case "endfilter":
-					case "endset":
-						while (stack.length && !stack[$-1].startsWith("%")) stack.popBack();
-						enforce(stack.endsWith("%" ~ t.kind[3 .. $]),
-							"Found " ~ t.kind ~ " but expected " ~ (stack.length ? "end" ~ stack[$-1][1..$] : "end-of-file")
-						);
-						stack.popBack();
-						break;
-					default:
-						break;
-				}
-			},
-			(ref const TokenDbtComment t)
-			{
-			},
-		);
-
-		wsPre = max(wsLast, wsPre);
-		if (!wsPre && wasWord && isWord)
-			wsPre = WhiteSpace.space;
-		if (!result.length)
-			wsPre = WhiteSpace.none;
-
-		final switch (wsPre)
+		foreach (tokenIndex, ref token; tokens)
 		{
-			case WhiteSpace.none:
-				break;
-			case WhiteSpace.space:
-				result ~= Token(TokenWhiteSpace(" "));
-				break;
-			case WhiteSpace.blankLine:
-				result ~= Token(TokenWhiteSpace("\n"));
-				goto case;
-			case WhiteSpace.newLine:
-				auto indent = stack.length;
-				if (indent && outdent) indent--;
-				result ~= Token(TokenWhiteSpace("\n" ~ "\t".replicate(indent)));
-				break;
+			WhiteSpace wsPre, wsPost;
+			void delegate()[] post;
+			bool isWord, outdent;
+
+			token.match!(
+				(ref const TokenWhiteSpace t)
+				{
+					assert(false);
+				},
+				(ref const TokenComment t)
+				{
+					wsPre = wsPost = WhiteSpace.newLine;
+				},
+				(ref const TokenKeyword t)
+				{
+					isWord = true;
+					switch (t.text)
+					{
+						case "AS":
+						case "AND":
+						case "OR":
+						case "NOT":
+						case "LIKE":
+						case "BETWEEN":
+						case "IN":
+						case "IS":
+						case "OVER":
+						case "THEN":
+						case "RETURNS":
+							wsPre = wsPost = WhiteSpace.space;
+							break;
+						case "SELECT":
+						case "SELECT AS STRUCT":
+							wsPre = wsPost = WhiteSpace.newLine;
+							if (stack.endsWith("WITH"))
+								stack.popBack();
+							post ~= { stack ~= "SELECT"; };
+							break;
+						case "FROM":
+						case "WHERE":
+						case "JOIN":
+						case "CROSS JOIN":
+						case "INNER JOIN":
+						case "LEFT JOIN":
+						case "LEFT OUTER JOIN":
+						case "RIGHT JOIN":
+						case "RIGHT OUTER JOIN":
+						case "GROUP BY":
+						case "ORDER BY":
+						case "HAVING":
+						case "QUALIFY":
+						case "PARTITION BY":
+							wsPre = wsPost = WhiteSpace.newLine;
+							if (stack.endsWith("SELECT"))
+								stack.popBack();
+							post ~= { stack ~= "SELECT"; };
+							break;
+						case "WINDOW":
+							wsPre = WhiteSpace.newLine;
+							if (stack.endsWith("SELECT"))
+								stack.popBack();
+							post ~= { stack ~= "SELECT"; };
+							break;
+						case "UNION ALL":
+						case "UNION DISTINCT":
+						case "INTERSECT DISTINCT":
+						case "EXCEPT DISTINCT":
+							wsPre = wsPost = WhiteSpace.newLine;
+							if (stack.endsWith("SELECT"))
+								stack.popBack();
+							outdent = true;
+							break;
+						case "WITH":
+							wsPre = wsPost = WhiteSpace.newLine;
+							post ~= { stack ~= "WITH"; };
+							break;
+						case "USING":
+							wsPre = WhiteSpace.newLine;
+							break;
+						case "CASE":
+							wsPre = wsPost = WhiteSpace.newLine;
+							post ~= { stack ~= "CASE"; };
+							break;
+						case "WHEN":
+						case "ELSE":
+							wsPre = WhiteSpace.newLine;
+							break;
+						case "END":
+							wsPre = WhiteSpace.newLine;
+							if (stack.endsWith("CASE"))
+								stack.popBack();
+							break;
+						default:
+							break;
+					}
+				},
+				(ref const TokenIdentifier t)
+				{
+					isWord = true;
+				},
+				(ref const TokenNamedParameter t)
+				{
+					isWord = true;
+				},
+				(ref const TokenOperator t)
+				{
+					switch (t.text)
+					{
+						case ".":
+							break;
+						case "(":
+							if (tokenIndex + 1 < tokens.length && tokens[tokenIndex + 1] == Token(TokenOperator(")")))
+								return; // Don't break up "()"
+							wsPost = WhiteSpace.newLine;
+							post ~= { stack ~= "("; };
+							break;
+						case ")":
+							if (tokenIndex && tokens[tokenIndex - 1] == Token(TokenOperator("(")))
+								return; // Don't break up "()"
+
+							wsPre = WhiteSpace.newLine;
+							stack = retro(find(retro(stack), "("));
+							enforce(stack.length, "Mismatched )");
+							stack = stack[0 .. $-1];
+							break;
+						case "[":
+							wsPost = WhiteSpace.newLine;
+							post ~= { stack ~= "["; };
+							break;
+						case "]":
+							wsPre = WhiteSpace.newLine;
+							stack = retro(find(retro(stack), "["));
+							enforce(stack.length, "Mismatched ]");
+							stack = stack[0 .. $-1];
+							break;
+						case ",":
+							if (stack.endsWith("<"))
+								wsPost = WhiteSpace.space;
+							else
+							if (stack.endsWith("WITH"))
+								wsPost = WhiteSpace.blankLine;
+							else
+								wsPost = WhiteSpace.newLine;
+							break;
+						case ";":
+							wsPost = WhiteSpace.blankLine;
+							while (stack.endsWith("SELECT") || stack.endsWith("WITH"))
+								stack.popBack();
+							break;
+						default:
+							// Binary operators and others
+							wsPre = wsPost = WhiteSpace.space;
+							break;
+					}
+				},
+				(ref const TokenAngleBracket t)
+				{
+					final switch (t.text)
+					{
+						case "<":
+							post ~= { stack ~= "<"; };
+							break;
+						case ">":
+							stack = retro(find(retro(stack), "<"));
+							enforce(stack.length, "Mismatched >");
+							stack = stack[0 .. $-1];
+							break;
+					}
+				},
+				(ref const TokenString t)
+				{
+					isWord = true;
+				},
+				(ref const TokenNumber t)
+				{
+					isWord = true;
+				},
+				(ref const TokenDbtStatement t)
+				{
+					wsPre = wsPost = WhiteSpace.newLine;
+					switch (t.kind)
+					{
+						case "for":
+						case "if":
+						case "macro":
+						case "filter":
+							post ~= { stack ~= "%" ~ t.kind; };
+							break;
+						case "set":
+							if (!t.text.canFind('='))
+								post ~= { stack ~= "%" ~ t.kind; };
+							break;
+						case "elif":
+						case "else":
+							while (stack.length && !stack[$-1].startsWith("%")) stack.popBack();
+							enforce(stack.endsWith("%if"),
+								"Found " ~ t.kind ~ " but expected " ~ (stack.length ? "end" ~ stack[$-1][1..$] : "end-of-file")
+							);
+							stack.popBack();
+							post ~= { stack ~= "%if"; };
+							break;
+						case "endfor":
+						case "endif":
+						case "endmacro":
+						case "endfilter":
+						case "endset":
+							while (stack.length && !stack[$-1].startsWith("%")) stack.popBack();
+							enforce(stack.endsWith("%" ~ t.kind[3 .. $]),
+								"Found " ~ t.kind ~ " but expected " ~ (stack.length ? "end" ~ stack[$-1][1..$] : "end-of-file")
+							);
+							stack.popBack();
+							break;
+						default:
+							break;
+					}
+				},
+				(ref const TokenDbtComment t)
+				{
+				},
+			);
+
+			whiteSpace[tokenIndex] = max(whiteSpace[tokenIndex], wsPre);
+			if (!whiteSpace[tokenIndex] && wasWord && isWord)
+				whiteSpace[tokenIndex] = WhiteSpace.space;
+
+			indent[tokenIndex] = stack.length;
+			if (indent[tokenIndex] && outdent)
+				indent[tokenIndex]--;
+
+			foreach (fun; post)
+				fun();
+
+			whiteSpace[tokenIndex + 1] = wsPost;
+			wasWord = isWord;
 		}
+	}
 
-		result ~= token;
-		foreach (fun; post)
-			fun();
-
-		wsLast = wsPost;
-		wasWord = isWord;
+	// Final pass: materialize WhiteSpace into TokenWhiteSpace
+	Token[] result;
+	foreach (i; 0 .. tokens.length)
+	{
+		if (i && whiteSpace[i])
+		{
+			final switch (whiteSpace[i])
+			{
+				case WhiteSpace.none:
+					break;
+				case WhiteSpace.space:
+					result ~= Token(TokenWhiteSpace(" "));
+					break;
+				case WhiteSpace.blankLine:
+					result ~= Token(TokenWhiteSpace("\n"));
+					goto case;
+				case WhiteSpace.newLine:
+					result ~= Token(TokenWhiteSpace("\n" ~ "\t".replicate(indent[i])));
+					break;
+			}
+		}
+		result ~= tokens[i];
 	}
 
 	if (tokens.length)
